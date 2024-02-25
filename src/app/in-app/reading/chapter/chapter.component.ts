e19a979e16dc987df5bdf968b1db8c8799ab3953
savas <savasusame@gmail.com>
2024-02-25 22:29:15 +0300

    remove jquery from chapter component

--- src/app/in-app/reading/chapter/chapter.component.ts
+++ src/app/in-app/reading/chapter/chapter.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LogoBasicComponent } from '../../../shared/components/logo-basic/logo-basic.component';
-import $ from 'jquery';
 import { TranslateDialogComponent } from '../../translate-dialog/translate-dialog.component';
 
 @Component({
@@ -8,7 +7,7 @@ import { TranslateDialogComponent } from '../../translate-dialog/translate-dialo
   standalone: true,
   imports: [LogoBasicComponent, TranslateDialogComponent],
   templateUrl: './chapter.component.html',
-  styleUrl: './chapter.component.scss'
+  styleUrl: './chapter.component.scss',
 })
 export class ChapterComponent {
   @Input() chapterData: any;
@@ -17,87 +16,76 @@ export class ChapterComponent {
   translation: any = null;
 
   close() {
-		this.closingEvent.emit(true);
-    console.log("selami")
-	}
-
-
-  ngOnInit(){
-
+    this.closingEvent.emit(true);
+    console.log('selami');
   }
 
-  ngAfterViewInit(){
+  ngOnInit() {}
 
-    $(".lang-cont").each(function(){
-      this.addEventListener('contextmenu', function(e) {
-        e.preventDefault();
-      });
-    });
+  onSelectText(event: Event): void {
+    let s = window.getSelection();
+    if (s && s.anchorNode && s.toString().length > 0) {
+      let range = s.getRangeAt(0);
+      let node: any = s.anchorNode;
+      let startOffset = range.startOffset;
+      let endOffset = range.endOffset;
+      let startNode: any = node;
+      let endNode: any = node;
 
-    $('.lang-cont').on("click touchstart", (e) => {
-      let s = window.getSelection();
-      if (s && s.anchorNode && s.toString().length > 0) {
-          let range = s.getRangeAt(0);
-          let node: any = s.anchorNode;
-          let startOffset = range.startOffset;
-          let endOffset = range.endOffset;
-          let startNode: any = node;
-          let endNode: any = node;
-
-          // Find start node and offset
-          while (startOffset > 0 && startNode.textContent[startOffset - 1] != ' ') {
-              startOffset -= 1;
-          }
-          if (startOffset == 0) {
-              startNode = startNode.previousSibling;
-              while (startNode && startNode.nodeType != Node.TEXT_NODE) {
-                  startNode = startNode.lastChild;
-              }
-              if (!startNode) {
-                  startNode = node;
-              } else {
-                  startOffset = startNode.textContent.length;
-              }
-          }
-
-          // Find end node and offset
-          while (endOffset < node.textContent.length && node.textContent[endOffset] != ' ') {
-              endOffset += 1;
-          }
-          if (endOffset == node.textContent.length) {
-              endNode = endNode.nextSibling;
-              while (endNode && endNode.nodeType != Node.TEXT_NODE) {
-                  endNode = endNode.firstChild;
-              }
-              if (!endNode) {
-                  endNode = node;
-              } else {
-                  endOffset = 0;
-              }
-          }
-
-          // Create new range and select it
-          let newRange = document.createRange();
-          newRange.setStart(startNode, startOffset);
-          newRange.setEnd(endNode, endOffset);
-          s.removeAllRanges();
-          s.addRange(newRange);
-
-          // Alert selected text
-          let str = newRange.toString().trim().replace(/[.,;"]/g, "");
-
-          this.translation = str;
-
-
+      // Find start node and offset
+      while (startOffset > 0 && startNode.textContent[startOffset - 1] != ' ') {
+        startOffset -= 1;
       }
-    });
+      if (startOffset == 0) {
+        startNode = startNode.previousSibling;
+        while (startNode && startNode.nodeType != Node.TEXT_NODE) {
+          startNode = startNode.lastChild;
+        }
+        if (!startNode) {
+          startNode = node;
+        } else {
+          startOffset = startNode.textContent.length;
+        }
+      }
+
+      // Find end node and offset
+      while (
+        endOffset < node.textContent.length &&
+        node.textContent[endOffset] != ' '
+      ) {
+        endOffset += 1;
+      }
+      if (endOffset == node.textContent.length) {
+        endNode = endNode.nextSibling;
+        while (endNode && endNode.nodeType != Node.TEXT_NODE) {
+          endNode = endNode.firstChild;
+        }
+        if (!endNode) {
+          endNode = node;
+        } else {
+          endOffset = 0;
+        }
+      }
+
+      // Create new range and select it
+      let newRange = document.createRange();
+      newRange.setStart(startNode, startOffset);
+      newRange.setEnd(endNode, endOffset);
+      s.removeAllRanges();
+      s.addRange(newRange);
+
+      // Alert selected text
+      let str = newRange
+        .toString()
+        .trim()
+        .replace(/[.,;"]/g, '');
+
+      this.translation = str;
+    }
   }
 
-
-
-
-  closeTranslate(){
-    if(window.getSelection){
+  closeTranslate() {
+    if (window.getSelection) {
       window.getSelection()?.removeAllRanges();
     }
     this.translation = null;
