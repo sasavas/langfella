2421efb619d8f3cf36b7ffddb84edcf49355e4f9
savas <savasusame@gmail.com>
2024-02-25 23:59:27 +0300

    remove jquery from import article component

--- src/app/in-app/import-articles/import-articles.component.ts
+++ src/app/in-app/import-articles/import-articles.component.ts
@@ -1,22 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import $ from 'jquery';
+import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 import { ArticleService } from '../../shared/services/articles.service';
-import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { LoadingComponent } from '../../shared/components/loading/loading.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-import-articles',
   standalone: true,
   imports: [FormsModule, MatIconModule, LoadingComponent],
   templateUrl: './import-articles.component.html',
-  styleUrl: './import-articles.component.scss'
+  styleUrls: ['./import-articles.component.scss']
 })
 export class ImportArticlesComponent {
-  test: any = "";
-  htmlUrl: any = "";
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('dropArea') dropArea!: ElementRef<HTMLDivElement>;
+  htmlUrl: string = "";
   error: string = "";
   loading: boolean = false;
 
@@ -27,57 +26,59 @@ export class ImportArticlesComponent {
     private router: Router
   ) {}
 
-  ngOnInit(){
-    let fileInput:any = document.getElementById('myFile')
-    console.log(fileInput);
-    let droparea = document.getElementsByClassName('file-drop-area')[0];
+  // Removed jQuery and direct DOM manipulation
 
-    // highlight drag area
-    $('.file-input').on('dragenter focus click', function() {
-      droparea.classList.add('is-active');
-    });
-
-    // back to normal state
-    $('.file-input').on('dragleave blur drop', function() {
-      droparea.classList.add('is-active');
-    });
-
-    fileInput.addEventListener('change', (e:any)=> {
-      let $textContainer =  $('.file-input').prev();
-      let extension = fileInput.files[0].type
-      if(extension!=="application/epub+zip"){
-        alert("wrong file type")
-        return
+  fileChanged(event: any) {
+    const files = event.target.files;
+    if (files.length > 0) {
+      const file = files[0];
+      if (file.type !== "application/epub+zip") {
+        alert("Wrong file type");
+        return;
       }
-      let fileName = fileInput.value.split('\\').pop();
-      $textContainer.text(fileName);
-      if(fileInput.value.length>0){
-        (droparea as HTMLElement).style.background = 'var(--success-1)';
-        (droparea as HTMLElement).style.color = 'white';
+      this.uploadFile(file);
+    }
+  }
+
+  uploadFile(file: File) {
+    this.loading = true;
+    const formData = new FormData();
+    formData.append('file', file);
+
+    this.articleService.importEpubFromFile(formData).subscribe({
+      next: (response) => {
+        this.loading = false;
+        this.router.navigate(['/app/read/' + response.id]);
+      },
+      error: (err) => {
+        this.loading = false;
+        this.error = err;
       }
-      this.loading = true;
-      const formData = new FormData();
-      formData.append('file', e.target.files[0]);
-
-      this.articleService.importEpubFromFile(formData).subscribe({
-        next: (response) => {
-          this.loading = false;
-          this.router.navigate(['/app/read/'+response.id])
-        },
-        error: (err) => {
-          //TO DO: SWITCH CASE ALL ERRORS
-          this.loading = false;
-          this.error = err;
-        }
-      });
-    })
-  }
-  afterRender(){
-  }
-  ngAfterViewInit(){
+    });
   }
 
-	close() {
-		this.closingEvent.emit(true);
-	}
+  highlightDropArea() {
+    this.dropArea.nativeElement.classList.add('is-active');
+  }
+
+  unhighlightDropArea() {
+    this.dropArea.nativeElement.classList.remove('is-active');
+  }
+
+  handleDrop(event: DragEvent) {
+    event.preventDefault();
+    this.unhighlightDropArea();
+    if (event.dataTransfer?.files) {
+      const file = event.dataTransfer.files[0];
+      if (file && file.type === "application/epub+zip") {
+        this.uploadFile(file);
+      } else {
+        alert("Wrong file type");
+      }
+    }
+  }
+
+  close() {
+    this.closingEvent.emit(true);
+  }
 }
